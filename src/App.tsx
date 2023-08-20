2a2665605c924e308b2fc9e77e5988cf0de67409
Anton <antoha_denisenkov@yandex.ru>
2023-08-20 19:31:14 +0500

    библиотека реакт роутер дом, подключил роуты к страницам

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
-export function App() {
-  return <div className="App"></div>;
-}
+import { useEffect } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+
+import { Layout } from './components/Layout/Layout';
+import { AdminPage } from './pages/adminPage/AdminPage';
+import { UserPage } from './pages/userPage/UserPage';
+
+export const App = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/user')
+    }
+  }, [pathname, navigate]);
+
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='admin' element={<AdminPage />} />
+        <Route path='user' element={<UserPage />} />
+      </Route>
+    </Routes>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import { store } from './store/store';
 import { App } from './App';
 import './index.css';
@@ -9,6 +11,8 @@ const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
